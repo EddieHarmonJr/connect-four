871fab00ca3a65baeec4fbc17233bf68d27bd4ae
EddieHarmonJr <eddieharmon@ac.c4q.nyc>
2020-12-29 00:28:21 -0500

    add methods to add board and tokens to the HTML

--- ObjectOrientedJavaScriptByExample/Stage1 - Introducing Four in a Row/Stage1Step3 - Planning the Classes/js/Token.js
+++ ObjectOrientedJavaScriptByExample/Stage1 - Introducing Four in a Row/Stage1Step3 - Planning the Classes/js/Token.js
@@ -4,4 +4,22 @@ class Token {
     this.id = `token-${index}-${owner.id}`;
     this.dropped = false;
   }
+
+  /**
+   * Gets associated htmlToken.
+   */
+  get htmlToken() {
+    return document.getElementById(this.id);
+  }
+  
+  /**
+   * Draws new HTML token.
+   */
+  drawHTMLToken() {
+    const token = document.createElement('div');
+    document.getElementById("game-board-underlay").appendChild(token);
+    token.setAttribute("id", this.id);
+    token.setAttribute("class", "token");
+    token.style.backgroundColor = this.owner.color;
+  }
 };
