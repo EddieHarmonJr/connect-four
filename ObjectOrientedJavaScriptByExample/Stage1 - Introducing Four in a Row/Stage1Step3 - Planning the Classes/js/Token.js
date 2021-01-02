190ae0469c4612eabe5f3f2ab81df68ddb12ac9c
EddieHarmonJr <eddieharmon@ac.c4q.nyc>
2021-01-01 23:40:22 -0500

    create move left, move right methods and columnLocation attribute to the Token class

--- ObjectOrientedJavaScriptByExample/Stage1 - Introducing Four in a Row/Stage1Step3 - Planning the Classes/js/Token.js
+++ ObjectOrientedJavaScriptByExample/Stage1 - Introducing Four in a Row/Stage1Step3 - Planning the Classes/js/Token.js
@@ -3,6 +3,7 @@ class Token {
     this.owner = owner;
     this.id = `token-${index}-${owner.id}`;
     this.dropped = false;
+    this.columnLocation = 0;
   }
 
   /**
@@ -12,14 +13,43 @@ class Token {
     return document.getElementById(this.id);
   }
   
-  /**
-   * Draws new HTML token.
-   */
+  /** 
+ * Gets left offset of html element.
+ * @return  {number}   Left offset of token object's htmlToken.
+ */
+  get offsetLeft() {
+      return this.htmlToken.offsetLeft;
+  }
+
+/**
+ * Draws new HTML token.
+ */
   drawHTMLToken() {
     const token = document.createElement('div');
     document.getElementById("game-board-underlay").appendChild(token);
     token.setAttribute("id", this.id);
     token.setAttribute("class", "token");
     token.style.backgroundColor = this.owner.color;
+  }
+
+/** 
+ * Moves html token one column to left.
+ */
+  moveLeft() {
+      if (this.columnLocation > 0) {
+          this.htmlToken.style.left = this.offsetLeft - 76;
+          this.columnLocation -= 1;
+      }
+  }
+
+/** 
+ * Moves html token one column to right.
+ * @param   {number}    columns - number of columns in the game board
+ */
+  moveRight() {
+      if (this.columnLocation < columns - 1) {
+          this.htmlToken.style.left = this.offsetLeft + 76;
+          this.columnLocation += 1;
+      }
   }
 };
